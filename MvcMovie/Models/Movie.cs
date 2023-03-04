67ea2c8fe6112f6cfe362a386f71e38a9befeac6
Vikingslord <ahsanulhaque1998@outlook.com>
2023-03-04 17:54:48 +0600

    Add project files.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Movie Name")]
+        public string Title { get; set; }
+
+        [DisplayName("Release Date")]
+        [DataType(DataType.DateTime)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Precision(18, 2)]
+        public decimal Price { get; set; }
+    }
+}
